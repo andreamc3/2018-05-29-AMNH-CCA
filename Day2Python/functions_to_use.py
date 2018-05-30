0b39ae010e930cf76eddfb2708384a8c0d4b9c1c
Andrea Mejia <amc@1A304-CL3.local>
2018-05-30 15:22:43 -0400

    Adding functions_to_use.py module

---
+++ Day2Python/functions_to_use.py
@@ -0,0 +1,8 @@
+''' This module contains some functions we will create'''
+
+def division_function_to_test(numerator,denominator):
+    '''Always returns the float value of dividend/divisor'''
+    return float(numerator)/denominator
+
+#Example attribute
+example_dividend=300
